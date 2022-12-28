d470e030c4fc26cc3dbde660f480cc92de131716
Nigel van Keulen <Nigelvk@itextra.nl>
2022-12-28 22:07:14 +0100

    Added tinygo http client

---
+++ requester/client_tiny.go
@@ -0,0 +1,84 @@
+//go:build js && wasm && tinygo
+// +build js,wasm,tinygo
+
+package requester
+
+import (
+	"github.com/Nigel2392/jsext/requester/fetch"
+)
+
+// This client works a little bit differently from the normal APIClient.
+// This is due to not everything being supported in TinyGo,
+// Or being too much of a hassle to implement.
+type APIClient struct {
+	Request *fetch.Request
+}
+
+func NewAPIClient() *APIClient {
+	return &APIClient{
+		Request: &fetch.Request{},
+	}
+}
+
+func (c *APIClient) Get(url string) *APIClient {
+	c.Request.Method = "GET"
+	c.Request.URL = url
+	return c
+}
+
+func (c *APIClient) Post(url string) *APIClient {
+	c.Request.Method = "POST"
+	c.Request.URL = url
+	return c
+}
+
+func (c *APIClient) Put(url string) *APIClient {
+	c.Request.Method = "PUT"
+	c.Request.URL = url
+	return c
+}
+
+func (c *APIClient) Patch(url string) *APIClient {
+	c.Request.Method = "PATCH"
+	c.Request.URL = url
+	return c
+}
+
+func (c *APIClient) Delete(url string) *APIClient {
+	c.Request.Method = "DELETE"
+	c.Request.URL = url
+	return c
+}
+
+func (c *APIClient) Head(url string) *APIClient {
+	c.Request.Method = "HEAD"
+	c.Request.URL = url
+	return c
+}
+
+// Add headers to the request
+func (c *APIClient) WithHeaders(headers map[string]string) *APIClient {
+	for k, v := range headers {
+		c.Request.Headers[k] = v
+	}
+	return c
+}
+
+// Add data to the request
+func (c *APIClient) WithData(formData map[string]interface{}, encoding Encoding, file ...File) *APIClient {
+	switch encoding {
+	case JSON:
+		c.Request.Headers["Content-Type"] = "application/json"
+		c.Request.Body = fetch.MarshalMap(formData)
+	case FORM_URL_ENCODED:
+		panic(FORM_URL_ENCODED + " is not supported yet!")
+	case MULTIPART_FORM:
+		panic(MULTIPART_FORM + " is not supported yet!")
+	}
+	return c
+}
+
+// Send the request
+func (c *APIClient) Do() *fetch.Response {
+	return fetch.Fetch(c.Request)
+}
---
+++ requester/fetch/fetch.go
@@ -0,0 +1,169 @@
+//go:build js && wasm && tinygo
+// +build js,wasm,tinygo
+
+package fetch
+
+import (
+	"fmt"
+	"strings"
+	"syscall/js"
+)
+
+type Request struct {
+	Body        []byte            `json:"body,omitempty"`
+	BodyUsed    bool              `json:"bodyUsed,omitempty"`
+	Cache       string            `json:"cache,omitempty"`
+	Credentials string            `json:"credentials,omitempty"`
+	Destination string            `json:"destination,omitempty"`
+	Headers     map[string]string `json:"headers,omitempty"`
+	Integrity   string            `json:"integrity,omitempty"`
+	Method      string            `json:"method,omitempty"`
+	Mode        string            `json:"mode,omitempty"`
+	Priority    string            `json:"priority,omitempty"`
+	Redirect    string            `json:"redirect,omitempty"`
+	Referrer    string            `json:"referrer,omitempty"`
+	URL         string            `json:"url,omitempty"`
+}
+
+func (f *Request) SetHeader(key, value string) {
+	if f.Headers == nil {
+		f.Headers = make(map[string]string)
+	}
+	f.Headers[key] = value
+}
+
+func (f *Request) Object() js.Value {
+	var jsRequest = js.Global().Call("eval", "new Object()")
+	if f.Headers != nil {
+		var jsMap = js.Global().Call("eval", "new Object()")
+		for key, value := range f.Headers {
+			jsMap.Set(key, value)
+		}
+		jsRequest.Set("headers", jsMap)
+	}
+	if f.Body != nil {
+		jsRequest.Set("body", string(f.Body))
+	}
+	if f.Method != "" {
+		jsRequest.Set("method", strings.ToUpper(f.Method))
+	}
+	if f.Cache != "" {
+		jsRequest.Set("cache", f.Cache)
+	}
+	if f.Credentials != "" {
+		jsRequest.Set("credentials", f.Credentials)
+	}
+	if f.Destination != "" {
+		jsRequest.Set("destination", f.Destination)
+	}
+	if f.Integrity != "" {
+		jsRequest.Set("integrity", f.Integrity)
+	}
+	if f.Mode != "" {
+		jsRequest.Set("mode", f.Mode)
+	}
+	if f.Priority != "" {
+		jsRequest.Set("priority", f.Priority)
+	}
+	if f.Redirect != "" {
+		jsRequest.Set("redirect", f.Redirect)
+	}
+	if f.Referrer != "" {
+		jsRequest.Set("referrer", f.Referrer)
+	}
+
+	return jsRequest
+
+}
+
+type Response struct {
+	Body       []byte
+	Headers    map[string]string
+	StatusCode int
+}
+
+// TinyGO fetch request implementation.
+func Fetch(options *Request) *Response {
+	if options.Method == "" {
+		options.Method = "GET"
+	}
+
+	switch options.Method {
+	case "GET", "HEAD":
+		options.Body = nil
+	case "":
+		options.Method = "GET"
+		options.Body = nil
+	}
+
+	var fetch = js.Global().Call("fetch", options.URL, options.Object())
+	if fetch.IsUndefined() {
+		panic("fetch is undefined")
+	}
+
+	var respChan = make(chan *Response)
+	fetch.Call("then", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		var response = args[0]
+		var headers = response.Get("headers")
+		var jsHeaders = make(map[string]string)
+		keys := headers.Call("keys")
+		for i := 0; i < keys.Length(); i++ {
+			var key = keys.Index(i).String()
+			var value = headers.Call("get", key).String()
+			jsHeaders[key] = value
+		}
+		var statusCode = response.Get("status").Int()
+		var resp = &Response{
+			Headers:    jsHeaders,
+			StatusCode: statusCode,
+		}
+		response.Call("text").Call("then", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+			var text = args[0].String()
+			resp.Body = []byte(text)
+			respChan <- resp
+			return nil
+		}))
+		return nil
+	}))
+	return <-respChan
+}
+
+func MarshalMap(data map[string]interface{}) []byte {
+	var startString = "{"
+	var endString = "}"
+	var dataString = ""
+	for key, value := range data {
+		var valueString = ""
+		//lint:ignore S1034 because we need to use the type of the value
+		switch value.(type) {
+		case string:
+			valueString = fmt.Sprintf("\"%v\"", value)
+		case bool:
+			valueString = fmt.Sprintf("%v", value)
+		case int:
+			valueString = fmt.Sprintf("%v", value)
+		case float64:
+			valueString = fmt.Sprintf("%v", value)
+		case []byte:
+			valueString = `"` + strings.ReplaceAll(string(value.([]byte)), `"`, `\"`) + `"`
+		case map[string]interface{}:
+			valueString = string(MarshalMap(value.(map[string]interface{}))) //`"` +  + `"`
+		case map[string]string:
+			valueString = string(MarshalMap(StringMapToAnyMap(value.(map[string]string))))
+		default:
+			valueString = fmt.Sprintf("\"%v\"", value)
+		}
+		dataString += fmt.Sprintf("\"%v\":%v,", key, valueString)
+	}
+	dataString = strings.TrimSuffix(dataString, ",")
+	return []byte(startString + dataString + endString)
+
+}
+
+func StringMapToAnyMap(input map[string]string) map[string]interface{} {
+	var output = make(map[string]interface{}, len(input))
+	for key, value := range input {
+		output[key] = value
+	}
+	return output
+}
