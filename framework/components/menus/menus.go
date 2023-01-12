a74b98034039cb7d089608018afcc1ab31214c91
Nigel van Keulen <Nigelvk@itextra.nl>
2023-01-12 16:27:42 +0100

    Added dropdown menu

--- framework/components/menus/menus.go
+++ framework/components/menus/menus.go
@@ -7,6 +7,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/Nigel2392/jsext"
 	"github.com/Nigel2392/jsext/framework/components"
 	"github.com/Nigel2392/jsext/framework/elements"
 	"github.com/Nigel2392/jsext/framework/helpers/csshelpers"
@@ -252,4 +253,159 @@ func MenuCurtainDrop(urls []components.URL, btnWidth int, curtainColor string, c
 	`)
 	// Return the menu container and the urlItems
 	return menu_container //, urlItems
+}
+
+type DropDownOptions struct {
+	Background     string
+	Color          string
+	BorderWidth    string
+	Width          string
+	Height         string
+	ButtonWidth    string
+	ButtonHeight   string
+	ButtonText     string
+	MenuItems      []*elements.Element
+	ItemsPerColumn int
+	Prefix         string
+}
+
+func (d *DropDownOptions) SetDefaults() {
+	if d.Background == "" {
+		d.Background = "#ffffff"
+	}
+	if d.Color == "" {
+		d.Color = "#333333"
+	}
+	if d.ButtonText == "" {
+		d.ButtonText = "Menu"
+	}
+	if d.Width == "" {
+		d.Width = "100%"
+	}
+	if d.Height == "" {
+		d.Height = "400px"
+	}
+	if d.ButtonWidth == "" {
+		d.ButtonWidth = "100%"
+	}
+	if d.ButtonHeight == "" {
+		d.ButtonHeight = "50px"
+	}
+	if d.ItemsPerColumn == 0 {
+		d.ItemsPerColumn = 8
+	}
+	if d.Prefix == "" {
+		d.Prefix = "jsext-dropdown-"
+	}
+	d.Width = "calc(" + d.Width + " - calc(" + d.BorderWidth + " * 2))"
+}
+
+func Dropdown(options DropDownOptions) *elements.Element {
+	if len(options.MenuItems) == 0 {
+		panic("DropDownMenu: No menu items provided")
+	}
+
+	options.SetDefaults()
+
+	var dropDownContainer = elements.Div().AttrClass(options.Prefix + "container")
+	var dropDownButton = dropDownContainer.Button(options.ButtonText).AttrClass(options.Prefix + "dropbtn")
+	var dropDownContentContainer = dropDownContainer.Div().AttrClass(options.Prefix + "content-container").AttrStyle("display:none")
+	var dropDownContent = dropDownContentContainer.Div().AttrClass(options.Prefix + "content")
+
+	dropDownButton.AddEventListener("click", func(this jsext.Value, event jsext.Event) {
+		if dropDownContentContainer.JSExtElement().Style().Display() == "block" {
+			dropDownContentContainer.JSExtElement().Style().Display("none")
+			dropDownButton.JSExtElement().ClassList().Remove(options.Prefix + "active")
+
+		} else {
+			dropDownContentContainer.JSExtElement().Style().Display("block")
+			dropDownButton.JSExtElement().ClassList().Add(options.Prefix + "active")
+		}
+	})
+
+	columns := len(options.MenuItems) / options.ItemsPerColumn
+	if len(options.MenuItems)%options.ItemsPerColumn != 0 {
+		columns++
+	}
+
+	for i := 0; i < columns; i++ {
+		var column = dropDownContent.Div().AttrClass(options.Prefix + "column" + strconv.Itoa(i))
+		for j := 0; j < options.ItemsPerColumn; j++ {
+			var index = i*options.ItemsPerColumn + j
+			if index >= len(options.MenuItems) {
+				break
+			}
+			column.Append(options.MenuItems[index].AttrClass(options.Prefix + "item"))
+		}
+	}
+
+	var css = `
+		.` + options.Prefix + `container {
+			position: relative;
+			display: inline-block;
+			width: ` + options.ButtonWidth + `;
+		}
+		.` + options.Prefix + `dropbtn {
+			background-color: ` + options.Background + `;
+			color: ` + options.Color + `;
+			padding: calc(` + options.ButtonHeight + ` / 2), calc(` + options.ButtonWidth + ` / 2);
+			font-size: calc(` + options.ButtonHeight + ` / 2);
+			border: none;
+			cursor: pointer;
+			width: ` + options.ButtonWidth + `;
+			height: ` + options.ButtonHeight + `;
+		}
+		.` + options.Prefix + `dropbtn:hover {
+			background-color: ` + options.Color + `;
+			color: ` + options.Background + `;
+		}
+		.` + options.Prefix + `content-container {
+			display: none;
+			position: absolute;
+			background-color: ` + options.Background + `;
+			min-width: ` + options.Width + `;
+			max-width: ` + options.Width + `;
+			min-height: ` + options.Height + `;
+			max-height: ` + options.Height + `;
+			overflow: auto;
+			z-index: 1;
+			border: ` + options.BorderWidth + ` solid ` + options.Color + `;
+		}
+		.` + options.Prefix + `content {
+			color: ` + options.Color + `;
+			text-decoration: none;
+			display: grid;
+			grid-template-columns: repeat(` + strconv.Itoa(columns) + `, 1fr);
+		}
+		.` + options.Prefix + `item {
+			background-color: ` + options.Background + `;
+			color: ` + options.Color + `;
+			font-size: 20px;
+			text-decoration: none;
+			display: block;
+			text-align: center;
+			height: calc(` + options.Height + ` / ` + strconv.Itoa(options.ItemsPerColumn) + `);
+			line-height: calc(` + options.Height + `/ ` + strconv.Itoa(options.ItemsPerColumn) + `);
+		}
+		.` + options.Prefix + `item:hover {
+			background-color: ` + options.Color + `;
+			color: ` + options.Background + `;
+		}
+		.` + options.Prefix + `active {
+			background-color: ` + options.Color + `;
+			color: ` + options.Background + `;
+		}
+		`
+
+	for i := 0; i < columns; i++ {
+		css += `
+			.` + options.Prefix + `column` + strconv.Itoa(i) + ` {
+				display:block;
+			}
+		`
+	}
+
+	dropDownContainer.StyleBlock(css)
+
+	return dropDownContainer
 }
