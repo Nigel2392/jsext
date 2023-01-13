db25df40987bcef5e8f1bda3c7e94844a911735f
Nigel van Keulen <Nigelvk@itextra.nl>
2023-01-13 10:30:15 +0100

    Improved image carousel

--- framework/components/misc/misc.go
+++ framework/components/misc/misc.go
@@ -42,6 +42,7 @@ var Box = []string{
 // Returns slice of elements
 // [0]: search container, [1]: search bar, [2]: search bar submit button
 func SearchBar(classPrefix, foregroundHex, background, text string) []*elements.Element {
+	classPrefix = classPrefix + helpers.FNVHashString(classPrefix+background+foregroundHex+text) + "-"
 	var searchContainer = elements.Div().AttrClass(classPrefix + "search-container")
 	var searchbar = searchContainer.Input("text", "search", text).AttrClass(classPrefix + "searchbar")
 	var searchBarSubmit = searchContainer.Button(text).AttrClass(classPrefix + "searchbar-submit")
@@ -55,36 +56,36 @@ func SearchBar(classPrefix, foregroundHex, background, text string) []*elements.
 	var b_G_str = strconv.Itoa(int(b_G))
 	var b_B_str = strconv.Itoa(int(b_B))
 
-	searchContainer.StyleBlock(`
-		.` + classPrefix + `search-container {
+	jsext.StyleBlock(classPrefix, `
+		.`+classPrefix+`search-container {
 			display: grid;
 			grid-template-columns: 3fr 1fr;
 			grid-template-areas: "searchbar submit";
 			grid-template-rows: 1fr;
 			column-gap: 3px;
 		}
-		.` + classPrefix + `searchbar {
+		.`+classPrefix+`searchbar {
 			height: 35px;
 			margin: 6px 0;
 			padding: 0 5px;
-			background-color: ` + background + `;
-			color: ` + foregroundHex + `;
-			border: 1px solid rgba(` + b_A_str + `, ` + b_G_str + `, ` + b_B_str + `, 0.5);
+			background-color: `+background+`;
+			color: `+foregroundHex+`;
+			border: 1px solid rgba(`+b_A_str+`, `+b_G_str+`, `+b_B_str+`, 0.5);
 			border-radius: 5px;
 			font-size: 20px;
 		}
-		.` + classPrefix + `searchbar:focus {
+		.`+classPrefix+`searchbar:focus {
 			outline: none;
 		}
-		.` + classPrefix + `searchbar-submit {
+		.`+classPrefix+`searchbar-submit {
 			grid-area: submit;
 			width: 100%;
 			height: 37px;
 			margin: 6px 0;
 			padding: 0 5px;
-			background-color: ` + background + `;
-			color: ` + foregroundHex + `;
-			border: 1px solid rgba(` + b_A_str + `, ` + b_G_str + `, ` + b_B_str + `, 0.5);
+			background-color: `+background+`;
+			color: `+foregroundHex+`;
+			border: 1px solid rgba(`+b_A_str+`, `+b_G_str+`, `+b_B_str+`, 0.5);
 			border-radius: 5px;
 			cursor: pointer;
 			font-size: 20px;
