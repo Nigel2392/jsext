45524ba4cb8f09827cc826f25077f9a7ec7b1140
Nigel van Keulen <Nigelvk@itextra.nl>
2023-01-18 15:56:45 +0100

    Websocket improvements

--- framework/requester/websocket/websocket.go
+++ framework/requester/websocket/websocket.go
@@ -31,25 +31,76 @@ func (w *WebSocket) Value() js.Value {
 func Open(url string, protocols ...string) (*WebSocket, error) {
 	var ws = New(url, protocols...)
 	var done = make(chan struct{})
-	ws.value.Call("addEventListener", "open", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+	var err = make(chan error)
+
+	var openFunc = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		ws.open = true
 		done <- struct{}{}
 		return nil
-	}))
-	ws.value.Call("addEventListener", "close", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		ws.open = false
-		return nil
-	}))
-	ws.value.Call("addEventListener", "error", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		ws.open = false
-		return nil
-	}))
+	})
+	ws.value.Call("addEventListener", "open", openFunc)
 
-	<-done
+	var closeFunc = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		ws.open = false
+		err <- eventToError(args[0])
+		return nil
+	})
+	ws.value.Call("addEventListener", "close", closeFunc)
+
+	select {
+	case <-done:
+		close(done)
+		close(err)
+	case e := <-err:
+		close(done)
+		close(err)
+		ws.value.Call("removeEventListener", "close", closeFunc)
+		ws.value.Call("removeEventListener", "open", openFunc)
+		return ws, e
+	}
+
+	ws.value.Call("removeEventListener", "close", closeFunc)
+	ws.value.Call("removeEventListener", "open", openFunc)
+
 	if ws.ReadyState() != SockOpen {
-		return nil, errors.New("websocket: failed to open")
+		return ws, errors.New("websocket: failed to open")
 	}
 	return ws, nil
+}
+
+func eventToError(event js.Value) error {
+	var reason string
+	switch event.Get("code").Int() {
+	case 1000:
+		reason = "Connection finished normally."
+	case 1001:
+		reason = "Endpoint moving away from the connection."
+	case 1002:
+		reason = "Protocol error occurred."
+	case 1003:
+		reason = "Invalid data received."
+	case 1004:
+		reason = "Reserved."
+	case 1005:
+		reason = "No status code received."
+	case 1006:
+		reason = "Connection aborted abnormally."
+	case 1007:
+		reason = "Invalid data type compared to specified type."
+	case 1008:
+		reason = "Policy violation."
+	case 1009:
+		reason = "Message size too large."
+	case 1010: // Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
+		reason = "An endpoint (client) is terminating the connection because it has expected the server to negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake. <br/> Specifically, the extensions that are needed are: " + event.Get("reason").String()
+	case 1011:
+		reason = "Unexpected condition prevented the request from being fulfilled."
+	case 1015:
+		reason = "Certificate error."
+	default:
+		reason = "Unknown reason"
+	}
+	return errors.New(reason)
 }
 
 func (w *WebSocket) IsOpen() bool {
