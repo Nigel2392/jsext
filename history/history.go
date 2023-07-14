03db177dfddaf81d3c5b7c8a7a9313dd6e7c2601
Nigel van Keulen <nigel@go-dev.nl>
2023-07-14 21:31:01 +0200

    updated history package, no need to call .Get()

--- history/history.go
+++ history/history.go
@@ -8,6 +8,7 @@ import (
 
 var history = js.Global().Get("history")
 
+// Call a method on the history object.
 func Call(method string, args ...interface{}) (js.Value, error) {
 	for i, arg := range args {
 		var v, err = jsc.ValueOf(arg)
@@ -19,22 +20,27 @@ func Call(method string, args ...interface{}) (js.Value, error) {
 	return js.Value(history).Call(method, args...), nil
 }
 
+// Get the length of the history object.
 func Length() int {
 	return history.Get("length").Int()
 }
 
+// Go to a specific page in the history.
 func Go(index int) {
 	history.Call("go", index)
 }
 
+// Go back one page.
 func Back() {
 	history.Call("back")
 }
 
+// Go forward one page.
 func Forward() {
 	history.Call("forward")
 }
 
+// Push a state to the history.
 func PushState(data interface{}, title string, url string) error {
 	var v, err = jsc.ValueOf(data)
 	if err != nil {
@@ -44,6 +50,7 @@ func PushState(data interface{}, title string, url string) error {
 	return nil
 }
 
+// Replace the current state in the history.
 func ReplaceState(data interface{}, title string, url string) error {
 	var v, err = jsc.ValueOf(data)
 	if err != nil {
@@ -53,10 +60,12 @@ func ReplaceState(data interface{}, title string, url string) error {
 	return nil
 }
 
+// Get the current state.
 func State() js.Value {
 	return history.Get("state")
 }
 
+// Scan the current state into dst.
 func ScanState(dst interface{}) error {
 	return jsc.Scan(State(), dst)
 }
